5fead25fd6f16ec7bcb8cfab121326e395b87b12
Rohith P <rohithprakah200623@gmail.com>
2026-02-04 06:16:00 +0000

    make more atrtractive

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 
 export const Header: FC = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full bg-black/30 backdrop-blur-md">
       <div className="container flex h-14 items-center">
         <div className="mr-4 flex">
           <a className="mr-6 flex items-center space-x-2" href="/">
